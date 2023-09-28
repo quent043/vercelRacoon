e6c104da526d8bce0da7276437c6034dd1995459
Quentin D.C <quent043@hotmail.com>
2023-09-28 16:10:00 +0100

    Rm config

--- next.config.js
+++ next.config.js
@@ -13,11 +13,6 @@ const withPWA = require('next-pwa')({
 });
 
 module.exports = withPWA({
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   webpack: (config, { webpack }) => {
     config.module.rules.push({
       test: /\.svg$/i,
