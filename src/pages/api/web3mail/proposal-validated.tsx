899ac13bb0d1026806a67494c07997a5ebb5cf4f
Quentin D.C <quent043@hotmail.com>
2023-10-02 19:06:55 +0100

    Test on build scripts

--- src/pages/api/web3mail/proposal-validated.tsx
+++ src/pages/api/web3mail/proposal-validated.tsx
@@ -29,9 +29,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   console.log('req.rawHeaders', req.rawHeaders);
   console.log('req', req);
 
-  // return res.status(200).json(`No new proposals validated available, ${req.query}, ${req.body}, ${req.headers}, ${req.rawHeaders}, ${req}`);
 
   prepareCronApi(chainId, platformId, mongoUri, cronSecurityKey, privateKey, res);
+
+  return res.status(200).json(`No new proposals validated available, ${req.query}, ${req.body}, ${req.headers}, ${req.rawHeaders}, ${req}`);
 
   await mongoose.connect(mongoUri as string);
 
